19f9be62039b698fe9ff589b03d22272a67ad977
Krasimira Georgieva <krassi77bg@gmail.com>
2018-12-12 16:29:40 +0200

    Implemented User CRUD functionality models, front-end libraries added

---
+++ src/AppBundle/Form/UserType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, array(
+                'label' => 'email:',
+                'attr' => array('class' => 'list-group list-group-item'),
+            ))
+            ->add('givenName', TextType::class, array(
+                'label' => 'given name:',
+                'attr' => array('class' => 'list-group list-group-item'),
+            ))
+            ->add('familyName', TextType::class, array(
+                'label' => 'family name:',
+                'attr' => array('class' => 'list-group list-group-item'),
+            ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => User::class,
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return '';
+
+    }
+}
